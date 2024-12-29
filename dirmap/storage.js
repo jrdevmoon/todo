80f7f55f6d23b77143c403afc1784eab059676c8
McRae Dev <mcrae.dev@proton.com>
2024-12-29 10:04:32 -0400

    29-12-2024_10:04

---
+++ dirmap/storage.js
@@ -0,0 +1,30 @@
+const DRIVE = class
+{
+  #root = __dirname
+  #config = require (`${this.#root}/config.json`)
+  #CONTAINER = require (`${this.#root}${this.#config.C.path}`)
+
+  #fs = require ('fs')
+  #path = require ('path')
+  
+  #driveName
+  #mainDir
+
+  constructor (prop)
+  {
+    if (prop)
+    {
+      if (prop.tag) this.tag = prop.tag
+      if (prop.path) this.root = prop.path
+      if (prop.monitor) this.monitor = prop.monitor
+        
+    }  
+    
+  }
+
+  #map (prop)
+  {
+    
+  }
+}
+module.exports = DRIVE
